c21ee0fe3d49f7cc71847e0cc7b84b5213562901
WKJay <1931048074@qq.com>
2020-04-20 07:06:03 +0800

    添加依赖

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import echarts from "echarts";
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts;
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 new Vue({
   render: h => h(App),
